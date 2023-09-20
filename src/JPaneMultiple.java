18d8f0d60b0390611236fff462010b5fb777eecb
Oscaruncode <oscarfuentesperezosp@gmail.com>
2023-09-20 12:24:11 -0500

    Pendiente y ecuación de la recta con dos puntos dados

--- src/JPaneMultiple.java
+++ src/JPaneMultiple.java
@@ -13,17 +13,26 @@ public class JPaneMultiple {
                 "y2", y2Field
         };
 
-        int option = JOptionPane.showConfirmDialog(null, message, "Cordenadas", JOptionPane.OK_CANCEL_OPTION);
+        boolean repeatInput = true;
+        while (repeatInput) {
+            int option = JOptionPane.showConfirmDialog(null, message, "Cordenadas", JOptionPane.OK_CANCEL_OPTION);
+            if (option == JOptionPane.OK_OPTION) {
 
-        if (option == JOptionPane.OK_OPTION) {
-            float x1 = Float.parseFloat(x1Field.getText());
-            float y1 = Float.parseFloat(y1Field.getText());
-            float x2 = Float.parseFloat(x2Field.getText());
-            float y2 = Float.parseFloat(y2Field.getText());
+                if( !x1Field.getText().isEmpty() && !y1Field.getText().isEmpty() && !x2Field.getText().isEmpty() && !y2Field.getText().isEmpty()){
+                    //Solo cuando se ha oprimido ok y los campos estan completos, el programa deja de ejecutar el ciclo y retorna el array
+                    float x1 = Float.parseFloat(x1Field.getText());
+                    float y1 = Float.parseFloat(y1Field.getText());
+                    float x2 = Float.parseFloat(x2Field.getText());
+                    float y2 = Float.parseFloat(y2Field.getText());
+                    return new float[]{x1, y1, x2, y2};
+                }
 
-            return new float[]{x1, y1, x2, y2};
-        } else {
-            return null; // Devuelve null si el usuario cancela el diálogo.
+            }
+
         }
+
+        return null;
     }
+
+
 }
--- src/LineDrawing.java
+++ src/LineDrawing.java
@@ -1,13 +1,12 @@
 import javax.swing.JFrame;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.geom.Line2D;
 
 public class LineDrawing extends JFrame{
 
     private final float[] coordenadas;
     public LineDrawing(float[] coors){
-        super("Pendiente y punto pendiente");
+        super("Pendiente y Ecuación de la recta");
         setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -19,20 +18,48 @@ public class LineDrawing extends JFrame{
     void drawLines(Graphics g){
 
         Graphics2D g2d = (Graphics2D) g;
-        //Eje Y
+        //Eje Y drawing
+        g2d.drawString("Eje Y", 250, 45);
         g2d.draw(new Line2D.Float(250.0f, 50f, 250.0f, 450f));
-        //Eje X
+        //Eje X drawing
+        g2d.drawString("Eje X", 455, 250);
         g2d.draw(new Line2D.Float(50f, 250f, 450.0f, 250f));
 
 
-        //System.out.println("("+x1+","+y1+")"+","+"("+x2+","+y2+")");
+        //Entrada cordenadas
+        float x1 = coordenadas[0];
+        float y1 = coordenadas[1];
+        float x2 = coordenadas[2];
+        float y2 = coordenadas[3];
+
+
+
+        //Calculo de pendiente y ecuación de la recta
+        float pendiente = (y2-y1)/(x2-x1);
+
+        float parteNumericaEcuacion = (pendiente * -x1) + y1;
+        String parteNumericaEcuacionYSigno = parteNumericaEcuacion>0? "+" + parteNumericaEcuacion: "" + parteNumericaEcuacion;
+        String parteConLiteral = pendiente + "x";
+        String ecuacion = parteConLiteral + parteNumericaEcuacionYSigno;
+
+        //Punto 1 , punto 2 , pendiente , ecuación show
+        g2d.drawString("Punto 1 = ( " + Math.round(x1) + " , " + Math.round(y1) + " )" , 50, 150);
+        g2d.drawString("Punto 2 = ( " + Math.round(x2) + " , " + Math.round(y2) + " )" , 50, 170);
+        g2d.drawString("Pendiente = " + pendiente, 50, 190);
+        g2d.drawString("Ecuación = " + ecuacion, 50, 210);
+
         // (0,0) point is at 250,250, so we need to add every coordinate for 250****and *-1 in y for resolve orientation problem
-        float x1 = (coordenadas[0])+250;
-        float y1 = (coordenadas[1]*-1)+250;
-        float x2 = (coordenadas[2])+250;
-        float y2 = (coordenadas[3]*-1)+250;
+        x1 = x1+250;
+        y1 = (y1*-1)+250;
+        x2 = x2+250;
+        y2 = (y2*-1)+250;
         //Line to draw:
+        g2d.setColor(Color.BLUE);
+        g2d.setStroke(new BasicStroke(4f));
+
+        //with exact point
         g2d.draw(new Line2D.Float(x1, y1, x2, y2));
+
 
      }
 
